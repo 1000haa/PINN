564c8765792c9778733108c99a34473145cb3e7f
1000haa <159397774+1000haa@users.noreply.github.com>
2024-02-09 01:39:57 +0900

    Add files via upload

--- a_PINN_Simple_ODE.py
+++ a_PINN_Simple_ODE.py
@@ -1,13 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import tensorflow as tf
 import random
-import warnings
-import os
 
 
-os.environ["TF_NUM_INTEROP_THREADS"] = "4"  # TensorFlow와 다른 라이브러리 간의 스레드 수
-os.environ["TF_NUM_INTRAOP_THREADS"] = "8"   # TensorFlow 내부의 연산 스레드 수
 
 
 
@@ -52,34 +49,59 @@ def ode_system(t,func_NN):
 train_t = (np.random.rand(30) * 2).reshape(-1, 1) #30개의 난수 생성후에 2를 곱해서 0~2 스케일링 (0~2초) 후에 열벡터로 변환
 train_loss_record = [] # loss 값 저장할 리스트 생성
 
-for itr in range(6000): #6000번 반복
+
+
+x = np.linspace(0, 2, 100)
+Pred= []
+
+Iteration=5000
+frame=10
+
+for itr in range(Iteration):
     with tf.GradientTape() as tape:
         train_loss = ode_system(train_t, NN) # 앞서 지정한 인공신경망에 생성한 난수 값을 입력하여 loss 값 계산
         train_loss_record.append(train_loss) # loss 값 저장
-
         grad_w = tape.gradient(train_loss, NN.trainable_variables) # 파라미터의 기울기 계산
         optm.apply_gradients(zip(grad_w, NN.trainable_variables)) #파라미터의 기울기 기반으로 파라미터 최적화
 
+    if itr % frame == 0:
+        Pred.append(NN.predict(x))
     if itr % 1000 == 0: #1000번 학습때 마다 loss값 출력
         print(train_loss.numpy())
 
-# Exact Solution과 비교
+
 plt.figure(figsize=(10, 8))
 plt.plot(train_loss_record)
 plt.show()
 
-test_t = np.linspace(0, 2, 100)
+fig = plt.figure(figsize=(10, 8))
+ax = plt.axes(xlim=(0, 2), ylim=(0.8, 1.2))
+line, = ax.plot([], [], linestyle='--', color='r', linewidth=2, label = 'PINN')
+
+def animate(i):
+    x = np.linspace(0, 2, 100)
+    y = Pred[i]
+    line.set_data(x, y)
+    return line,
 
 train_u = np.sin(2*np.pi*train_t)/(2*np.pi) + 1
-true_u = np.sin(2*np.pi*test_t)/(2*np.pi) + 1
-pred_u = NN.predict(test_t).ravel()
+true_u = np.sin(2*np.pi*x)/(2*np.pi) + 1
 
-plt.figure(figsize = (10,8))
 plt.plot(train_t, train_u, 'ok', label = 'Train')
-plt.plot(test_t, true_u, '-k',label = 'True')
-plt.plot(test_t, pred_u, '--r', label = 'Prediction')
-plt.legend(fontsize = 15)
+plt.plot(x, true_u, '-k',label = 'True')
+plt.legend(loc='upper right',fontsize = 15)
 plt.xlabel('t', fontsize = 15)
 plt.ylabel('u', fontsize = 15)
-plt.show()
+
+anim = FuncAnimation(fig, animate, frames=(int(Iteration/frame)), interval=0.01)
+
+
+
+anim.save('animation.gif', writer='pillow')
+
+
+
+
+
+
 
